80bf0b05cea5898b233324eb489b63f8b989432f
Stephanie Messad <sabud1208@gmail.com>
2018-11-26 05:03:04 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(sentence1)
-  "Hello there, and how are you?".each_char do |reverse|
-
+  "Hello there, and how are you?".each_char do |sentence1|
+  sentence1.reverse
   end
 end
